07c2a4673c446dfae45f00b95388dd1ee3b90288
Link4Electronics <link4electronics@gmail.com>
2026-05-20 01:24:45 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,31 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
-
+pacman -Syu --noconfirm \
+    cmake      	   \
+    fluidsynth 	   \
+	pipewire-alsa  \
+	pipewire-audio \
+	pipewire-jack  \
+    sdl2_mixer
+    
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Building Shockolate..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/Interrupt/systemshock"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --depth 1 "$REPO" ./systemshock
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+cmake -S ./systemshock -B build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+    -DENABLE_SDL2=ON \
+	-DENABLE_FLUIDSYNTH=ON \
+	-DENABLE_SOUND=ON
+cmake --build build -j$(nproc)
+mv -v build/systemshock ./AppDir/bin
