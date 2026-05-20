b1cb8e5e965f642bc0991b4fb9b842b427d5f538
Link4Electronics <link4electronics@gmail.com>
2026-05-20 01:36:06 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun ./AppDir/bin/shockolate /usr/lib/libfluidsynth.so*
+quick-sharun ./AppDir/bin/systemshock /usr/lib/libfluidsynth.so*
 
 # Additional changes can be done in between here
 
