8ebba063e336bbcd5f05781ac8835529942a1b93
Link4Electronics <link4electronics@gmail.com>
2026-05-20 03:02:12 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,9 +5,10 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.hook"
+export ADD_HOOKS="self-updater.hook:sdl-soundfonts.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export DEPLOY_OPENGL=1
+export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/systemshock /usr/lib/libfluidsynth.so*
